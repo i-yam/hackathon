914c4423e62d3834d8519033d7df84d589eacccd
Keno <k.schuerger2004@gmail.com>
2026-07-14 08:31:54 +0200

    2026: link to full solution repo instead of code folder
    Per maintainer request: replace the code folder with a link in 2026/readme.md pointing to the standalone repo (schallschutz-din4109).

--- 2026/readme.md
+++ 2026/readme.md
@@ -1,2 +1,3 @@
 Commit your solutions here
 https://github.com/beyzabetulay/planfuge.git
+https://github.com/Keno-fsdf/schallschutz-din4109.git
--- 2026/schallschutz-din4109/src/schallschutz/extraction/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Stufe 1 - Extraktion: Plan (PDF/Bild) -> Gebaeudemodell.
-
-- extrahiere_modell:    Einzelbild (sauberer Plan / Demo)
-- extrahiere_real_plan: gekachelt + legenden-bewusst (echte Ausfuehrungsplaene, dichte A1)
-"""
-from .claude_vision import extrahiere_modell, pdf_to_images, plan_to_images
-from .real_plan import extrahiere_real_plan
-
-__all__ = ["extrahiere_modell", "extrahiere_real_plan", "pdf_to_images", "plan_to_images"]
